1d7b4420fafd715d9db526bcb2535786b06047c8
Crapola <crapola@users.noreply.github.com>
2024-01-17 17:13:31 +0100

    Version 1.0.2

--- blender_t3d/__init__.py
+++ blender_t3d/__init__.py
@@ -2,7 +2,7 @@
 bl_info={
 	"name": "Import and export old Unreal .T3D format",
 	"author": "Crapola",
-	"version": (1,0,1),
+	"version": (1,0,2),
 	"blender": (3,3,0),
 	"location": "File > Import-Export ; Object",
 	"description": "Import and export UnrealED .T3D files.",
--- test/archive.ps1
+++ test/archive.ps1
@@ -1,4 +1,4 @@
 # Create release archive.
-Remove-Item blender_t3d/__pycache__ -ErrorAction Ignore
+Remove-Item blender_t3d/__pycache__, blender_t3d/.pytest_cache -ErrorAction Ignore
 Compress-Archive -Force -Path blender_t3d -DestinationPath blender_t3d.zip
 Write-Output Ok
