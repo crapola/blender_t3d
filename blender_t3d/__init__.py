607ecb1b5e8dd7c1a0ce807edfacc445973903ac
Crapola <crapola@users.noreply.github.com>
2024-02-17 18:08:35 +0100

    Version 1.1.0

--- blender_t3d/__init__.py
+++ blender_t3d/__init__.py
@@ -2,7 +2,7 @@
 bl_info={
 	"name": "Import and export old Unreal .T3D format",
 	"author": "Crapola",
-	"version": (1,0,8),
+	"version": (1,1,0),
 	"blender": (3,3,0),
 	"location": "File > Import-Export ; Object",
 	"description": "Import and export UnrealED .T3D files.",
