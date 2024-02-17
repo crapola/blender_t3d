ac7df18e2bca87be0820eb8c8273f28e307864fc
Crapola <crapola@users.noreply.github.com>
2024-02-17 17:46:54 +0100

    Update exporter

--- blender_t3d/exporter.py
+++ blender_t3d/exporter.py
@@ -5,7 +5,7 @@ import math
 
 import bmesh
 import bpy
-from mathutils import Euler, Matrix, Vector
+from mathutils import Euler, Matrix, Vector,geometry
 
 try:
 	from .t3d import Brush, Polygon, Vertex
@@ -19,109 +19,6 @@ if DEBUG:
 	_print=print
 
 TEXTURE_SIZE:float=256.0
-
-def basis_xform(a:Matrix,b:Matrix)->Matrix:
-	""" Get transform a -> b. """
-	return b@a.inverted_safe() # type: ignore
-
-def basis_from_points(o:Vector,x:Vector,y:Vector)->Matrix:
-	"""
-	2D Basis.
-	"""
-	m=Matrix( ((1,0,0),(0,1,0),(0,0,1)) )
-	m[0].xy=x-o #X
-	m[1].xy=y-o #Y
-	m[2].xy=o # Origin
-	m.transpose()
-	return m
-
-def get_material_name(obj,material_index:int)->str:
-	""" Get material name using index. """
-	return obj.data.materials[material_index].name if len(obj.data.materials)>0 else ""
-
-def normal_rotation(n:Vector)->Matrix:
-	""" Return the 3x3 matrix that rotates the Z=0 plane towards n. """
-	#https://math.stackexchange.com/a/1957132
-	l:float=math.sqrt(n.x**2+n.y**2)
-	if l==0:
-		return Matrix.Identity(3)
-	return Matrix([[n.y/l,-n.x/l,0],[n.x*n.z/l,n.y*n.z/l,-l],[n.x,n.y,n.z]])
-
-def flat_face(verts:list[Vector])->tuple[Vector,...]:
-	"""
-	Project three 3D points onto their 2D plane.
-	The point are respectively, origin, x axis, y axis.
-	Return list of three 2D points. First is always (0,0).
-	"""
-	assert len(verts)==3,"Requires three vectors."
-	# Define the plane.
-	po:Vector=verts[0] # Origin of the plane.
-	px:Vector=verts[1]-po # First direction vector.
-	py:Vector=verts[2]-po # Second direction vector.
-	px.normalize()
-	# Gram-Schmidt process.
-	py=py-px.dot(py)*px
-	py.normalize()
-	return tuple(Vector((px.dot(v-po),py.dot(v-po))) for v in verts)
-
-def transpose(matrix:Matrix)->Matrix:
-	""" mathutils.Matrix.transpose only works on square. """
-	return Matrix([[row[i] for row in matrix] for i in range(len(matrix[0]))])
-
-def export_uv(verts:list[Vector],uvs:list[Vector],normal:Vector)->Matrix:
-	""" Export UVs. """
-	v2d:tuple[Vector,...]=flat_face(verts)
-	uv_basis:Matrix=basis_from_points(*uvs)
-	vert_basis:Matrix=basis_from_points(*v2d)
-	transform:Matrix=basis_xform(vert_basis,uv_basis)
-	print("verts=",verts)
-	print("uvs=",uvs)
-	print("verts2d=",v2d)
-	print("transform=",transform)
-	m:Matrix=transform.transposed()
-	temp:Vector=m[0].copy()
-	m[0]=m[2].copy()
-	m[2]=temp
-	# The matrix m holds the 2D affine transform on the Z=0 plane.
-	# [ ox oy oz ]
-	# [ ux uy  0 ]
-	# [ vx vy  0 ]
-	# Now, we use the polygon normal to rotate it into 3D.
-	print(f"Face's normal is {normal}.")
-	r:Matrix=normal_rotation(normal)
-	print(f"Plane rotation: {r}")
-	m=m@r
-	print(f"3D transform:{m}")
-	return m
-
-def polygon_texture_transform(face:'bmesh.types.BMFace',mesh:'bmesh.types.BMesh')->tuple:
-	""" Compute the Origin, TextureU, TextureV for a given face. """
-	points:list[bmesh.types.BMLoop]=face.loops[0:3]
-
-	if len(points)<2 or len(mesh.loops.layers.uv)==0:
-		print("Invalid geometry or no UV map.")
-		return ((0,0,0),(1,0,0),(0,1,0))
-
-	uvmap=mesh.loops.layers.uv[0]
-	verts:list[Vector]=[x.vert.co for x in points] # type: ignore
-	uvs:list[Vector]=[x[uvmap].uv for x in points] # type: ignore
-
-	#uvs=[Vector((x.x,x.y)) for x in uvs]
-
-	m:Matrix=export_uv(verts,uvs,face.normal)*TEXTURE_SIZE
-
-	# m is a 3x3 matrix expected to contain
-	# [  -Origin-  ]
-	# [ -TextureU- ]
-	# { -TextureV- ]
-
-	print("M=",m)
-
-	origin:tuple
-	tu:tuple
-	tv:tuple
-	origin,tu,tv=m[0].to_tuple(),m[1].to_tuple(),m[2].to_tuple()
-	return origin,tu,tv
 
 def brush_from_object(o:'bpy.types.Object',scale_multiplier:float=1.0)->Brush|str:
 	""" Turn Blender Object into t3d.Brush. """
@@ -180,3 +77,79 @@ def export(object_list,scale_multiplier:float=1.0)->str:
 	if t3d_text:
 		t3d_text=f"""Begin Map\n{t3d_text}End Map\n"""
 	return t3d_text
+
+def export_uv(verts:list[Vector],uvs:list[Vector],normal:Vector)->tuple:
+	""" Return Origin,TextureU,TextureV in tuple. """
+	uvs=[Vector((uv.x,1-uv.y))*TEXTURE_SIZE for uv in uvs]
+	verts=rotate_triangle_towards_normal(verts,Vector((0,0,1)))
+	_print("Rotated verts to XY plane:",verts)
+	height=verts[0].z
+	verts=[v.xy for v in verts]
+	_print("Flat Verts:",verts)
+	m_uvs=Matrix((*[v.to_3d()+Vector((0,0,1)) for v in uvs],))
+	m_verts=Matrix((*[v.to_3d()+Vector((0,0,1)) for v in verts],))
+	_print("m_uvs:",m_uvs)
+	_print("m_verts:",m_verts)
+	m_uvs_inverse:Matrix=m_uvs.inverted_safe()
+	m_verts_inverse:Matrix=m_verts.inverted_safe()
+	_print("m_verts_inverse:",m_verts_inverse)
+	_print("m_uvs_inverse:",m_uvs_inverse)
+	u2v:Matrix=m_verts_inverse@m_uvs
+	_print("Result UVs to Verts transform:",u2v)
+	rot:Matrix=normal_rotation(Vector((0,0,1)),normal)
+	tu:Vector=u2v.col[0].xy
+	tv:Vector=u2v.col[1].xy
+	o:Vector=u2v[2].xy
+	tu=rot@tu.to_3d()
+	tv=rot@tv.to_3d()
+	# TODO: Origin doesn't come out right.
+	o=rot@o.to_3d()
+	return o,tu,tv
+
+def get_material_name(obj,material_index:int)->str:
+	""" Get material name using index. """
+	return obj.data.materials[material_index].name if len(obj.data.materials)>0 else ""
+
+def normal_rotation(n1:Vector,n2:Vector)->Matrix:
+	""" Rotation matrix between normals n1 to n2. """
+	assert n1!=Vector((0,0,0)) and n2!=Vector((0,0,0))
+	# Angle between two.
+	cos_theta:float=n1.dot(n2)/(n1.length*n2.length)
+	try:
+		theta:float=math.acos(cos_theta)
+	except ValueError:
+		theta=0
+	# Rotation axis.
+	axis:Vector=n1.cross(n2)
+	axis.normalize()
+	# Calculate rotation matrix using Rodrigues' formula.
+	axis_skew=Matrix((
+		(0, -axis.z, axis.y),
+		(axis.z, 0, -axis.x),
+		(-axis.y, axis.x, 0)
+	))
+	i:Matrix=Matrix.Identity(3)
+	r:Matrix=i+axis_skew*math.sin(theta)+axis_skew@axis_skew*(1-math.cos(theta))
+	return r
+
+def polygon_texture_transform(face:'bmesh.types.BMFace',mesh:'bmesh.types.BMesh')->tuple:
+	""" Compute the Origin, TextureU, TextureV for a given face. """
+	points:list[bmesh.types.BMLoop]=face.loops[0:3]
+	if len(points)<2 or len(mesh.loops.layers.uv)==0:
+		print("Invalid geometry or no UV map.")
+		return ((0,0,0),(1,0,0),(0,1,0))
+	uvmap=mesh.loops.layers.uv[0]
+	verts:list[Vector]=[x.vert.co for x in points] # type: ignore
+	uvs:list[Vector]=[x[uvmap].uv for x in points] # type: ignore
+	return export_uv(verts,uvs,face.normal)
+
+def rotate_triangle_towards_normal(points:list[Vector],n:Vector)->list:
+	""" Return points after plane is rotated towards n. """
+	assert len(points)==3,"Not a triangle."
+	plane_normal:Vector=geometry.normal(points)
+	rotation:Matrix=normal_rotation(plane_normal,n)
+	return [rotation@p for p in points]
+
+def transpose(matrix:Matrix)->Matrix:
+	""" mathutils.Matrix.transpose only works on square. """
+	return Matrix([[row[i] for row in matrix] for i in range(len(matrix[0]))])
